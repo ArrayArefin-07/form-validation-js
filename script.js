75572f21a313a86704dfed4cbccf538001a671bf
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-12-10 08:27:41 +0600

    Added functionality for validate Name

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ var phoneError = document.getElementById('phone-error');
 var emailError = document.getElementById('email-error');
 var messageError = document.getElementById('message-error');
 
+function validateName(){
+  var name = document.getElementById('contact-name').value;
+
+  if(name.length == 0){
+    nameError.innerHTML = 'Name is Required';
+    return false;
+  }
+  if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+    nameError.innerHTML = 'Write FUll Name Ex:Mobasher Arefin';
+    return false;
+  }
+  nameError.innerHTML = 'Valid';
+}

eb4693391c946e4aa544e7d4d38e753578af7051
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-12-10 08:33:33 +0600

    Added check sign icon
    And added style on it

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-var nameError =  document.getElementById('name-error');
-var phoneError = document.getElementById('phone-error');
-var emailError = document.getElementById('email-error');
-var messageError = document.getElementById('message-error');
+var nameError = document.getElementById("name-error");
+var phoneError = document.getElementById("phone-error");
+var emailError = document.getElementById("email-error");
+var messageError = document.getElementById("message-error");
 
-function validateName(){
-  var name = document.getElementById('contact-name').value;
+function validateName() {
+  var name = document.getElementById("contact-name").value;
 
-  if(name.length == 0){
-    nameError.innerHTML = 'Name is Required';
+  if (name.length == 0) {
+    nameError.innerHTML = "Name is Required";
     return false;
   }
-  if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
-    nameError.innerHTML = 'Write FUll Name Ex:Mobasher Arefin';
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+    nameError.innerHTML = "Write FUll Name Ex:Mobasher Arefin";
     return false;
   }
-  nameError.innerHTML = 'Valid';
+
+  nameError.innerHTML = '<i class="fa-solid fa-check-circle"></i>';
 }

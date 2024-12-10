4de960d4856c662537174a6c48e20100ef60dd08
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-12-10 08:16:29 +0600

    define all variable using queryselector

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var nameError = document.getElementById('name-error');
+var nameError =  document.getElementById('name-error');
 var phoneError = document.getElementById('phone-error');
 var emailError = document.getElementById('email-error');
 var messageError = document.getElementById('message-error');
